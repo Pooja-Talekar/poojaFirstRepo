392420ce3b9f2b2d00bcb4db0ddc86ecd0a0b614
Pooja <pooja.m.talekar@gmail.com>
2023-02-02 19:09:30 +0530

    New MyClass2ForGitHub class created in MyPractice1 Package

---
+++ src/MyPractice1/MyClass2forGitHub.java
@@ -0,0 +1,22 @@
+package MyPractice1;
+
+public class MyClass2forGitHub
+{
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			// Variable declaration  (Allocating /Reserving memory)
+			String name ;
+			String city;
+			// Variable Initialization (Assign Value)
+			name ="Pooja Talekar";
+			city ="Pune";
+			
+			System.out.println("Hi All"); //to print message
+			System.out.println("How Are you?"); //to print message
+			System.out.println(name); // to print value of variable 
+			System.out.println("Name"); // to print message (message in double quote)
+			System.out.println("My name is "+name);//"+" is used to concatenate the two statements and my name is common thing for everyone
+			System.out.println("I live in "+city);
+		}
+
+}
